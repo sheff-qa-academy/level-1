26f67908e541e64d23fda136a4b1c0e2dea0d79b
vsheff <v.sheff@8bitgroup.com>
2020-09-06 18:38:18 +0300

    Test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import com.opencsv.CSVWriter;
 import net.bytebuddy.asm.Advice;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -6,12 +7,16 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
 //        System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
         System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
@@ -22,20 +27,46 @@ public class Main {
 //        driverFF.get("https://market.yandex.ru");
 
         driverChrome.get("https://market.yandex.ru");
-        driverChrome.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//        driverChrome.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driverChrome.manage().window().maximize();
-        driverChrome.findElement(By.xpath("//span[@class='_14Uuc5WvKg' and text()='Войти']/parent::a")).click();
+//        driverChrome.findElement(By.xpath("//span[.='Войти']")).click();
+//
+//        ArrayList<String> handles = new ArrayList<String>(driverChrome.getWindowHandles());
+//        driverChrome.switchTo().window(handles.get(1));
+//
+//        driverChrome.findElement(By.cssSelector("#passp-field-login")).sendKeys("sheff-qa");
+//        driverChrome.findElement(By.xpath("//button[@data-t='button:action']")).click();
+//        driverChrome.findElement(By.xpath("//input[@name='passwd']")).sendKeys("Uxw29qwergt!$");
+//        driverChrome.findElement(By.xpath("//input[@name='passwd']")).submit();
 
-        ArrayList<String> handles = new ArrayList<>(driverChrome.getWindowHandles());
-        driverChrome.switchTo().window(handles.get(1));
-        driverChrome.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driverChrome.findElement(By.xpath("//input[@name='login']")).sendKeys("sheff-qa");
-        driverChrome.findElement(By.xpath("//button[@data-t='button:action']")).click();
-        driverChrome.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driverChrome.findElement(By.xpath("//input[@name='passwd']")).sendKeys("Uxw29qwergt!$");
-        driverChrome.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driverChrome.findElement(By.xpath("//input[@name='passwd']")).sendKeys(Keys.ENTER);
-        driverChrome.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
+        List<WebElement> popularCategories = driverChrome.findElements(By.xpath("//div[1]/div[@data-zone-name='category-link']/div/a"));
+
+        driverChrome.get(popularCategories.get(0).getAttribute("href"));
+
+        driverChrome.get("https://market.yandex.ru");
+
+        if (driverChrome.findElements(By.xpath("//span[.='Каталог товаров']/../parent::button/")).size() > 0) {
+
+            driverChrome.findElement(By.xpath("//span[.='Каталог товаров']/../parent::button/")).click();
+        } else {
+            driverChrome.findElement(By.xpath("/html/body/div[2]/div[3]/noindex/div/div/div[2]/div")).click();
+        }
+
+        List<WebElement> allCategories =
+                driverChrome.findElements(By.xpath("//div[@data-zone-name='category-link']/button/a/span"));
+
+        String csv = "data.csv";
+        CSVWriter writer = new CSVWriter(new FileWriter(csv));
+        String[] record = new String[allCategories.size()];
+        for (int i = 0; i < allCategories.size(); i++) {
+            record[i] = allCategories.get(i).getAttribute("textContent");
+        }
+        writer.writeNext(record);
+        writer.close();
+
+
+        //sendKeys(Keys.ENTER);
+
 //        driverChrome.quit();
     }
 }
