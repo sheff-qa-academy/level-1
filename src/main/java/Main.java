37c3875209b837aa5831730cfc294c8a22bc74ab
MeisterSky <sheff.victor@gmail.com>
2020-09-09 13:21:23 +0300

    refactored

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,7 +15,7 @@ public class Main {
 
 
 //        System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
-//        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
 
 //        WebDriver driverFF = new FirefoxDriver();
 
